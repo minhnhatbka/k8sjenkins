443fb82c342933eacbb6f71a435eabdc39299d5c
minhnhatbka <minhnhatbka@gmail.com>
2019-08-14 14:50:29 +0700

    fix

--- startup.sh
+++ startup.sh
@@ -8,9 +8,6 @@ DATE_FORMAT="+%Y-%m-%d %H:%M:%S"
 echo "Hello from nhattm2"
 echo "Current environment is" $env_name
 echo "Current application name is" $app_name
-echo "Current country code is" $country_code
-echo "Current java xmx is" $java_xmx
-echo "Current java xms is" $java_xms
 
 app_version=$(cat /image_info/app_version)
 git_revision=$(cat /image_info/git_revision)
@@ -19,7 +16,7 @@ config_url="http://192.168.50.11:31991/minio"
 echo "$(date "${DATE_FORMAT}") | Pulling Configuration"
 cd /tmp
 #response_http=$(wget --server-response ${config_url}/${app_name}-${app_version}.zip 2>&1 | awk '/^  HTTP/{print $2}')
-response_http=$(wget --server-response ${config_url}/alpha_104.zip 2>&1 | awk '/^  HTTP/{print $2}')
+response_http=$(wget --server-response http://192.168.50.11:31991/minio/alpha_106.zip 2>&1 | awk '/^  HTTP/{print $2}')
 echo "$(date "${DATE_FORMAT}") | Http status when pulling configuration is ${response_http}"
 if [[ ${response_http} != 200 ]]; then
 	echo "$(date "${DATE_FORMAT}") | Failed : Cannot pull configure"
@@ -28,7 +25,8 @@ if [[ ${response_http} != 200 ]]; then
 else
 	echo "$(date "${DATE_FORMAT}") | Pulled Configuration"
 	echo "$(date "${DATE_FORMAT}") | Extract Configuration file"
-	tar -zxvf $app_name-$app_version.zip && mv /tmp/$app_name-$app_version/* /data/projects/$app_name/config/
+	#tar -zxvf $app_name-$app_version.zip && mv /tmp/$app_name-$app_version/* /data/projects/$app_name/config/
+	tar -zxvf alpha_106.zip && mv /tmp/alpha_106/* /data/projects/$app_name/config/
 	echo "$(date "${DATE_FORMAT}") | Extracted Configuration file"
 	echo "$(date "${DATE_FORMAT}") | Check script file for get sensitive data from vault in folder config."	
 
