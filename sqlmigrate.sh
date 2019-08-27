911df1c01e8a9153c94a33aed9f1a80ae296f5f2
minhnhatbka <minhnhatbka@gmail.com>
2019-08-28 00:07:42 +0700

    fix

--- sqlmigrate.sh
+++ sqlmigrate.sh
@@ -3,16 +3,21 @@ app_name=$2
 
 cd ..
 
-unzip k8sjenkins/flyway.zip -d k8sjenkins
+unzip -qq k8sjenkins/flyway.zip -d k8sjenkins
 cp db/$env_version/$app_name/conf/flyway.conf k8sjenkins/flyway/conf/flyway.conf
 cp -R db/alpha/demo/sql/ k8sjenkins/flyway/
 chmod 755 ./k8sjenkins/flyway/flyway
+./k8sjenkins/flyway/flyway repair
 ./k8sjenkins/flyway/flyway migrate &> flyway.log
 
-if grep -q Successfully flyway.log
+if grep -q "Successfully applied" flyway.log
 then
-        echo "ok";
+        echo "Successfully applied";
+elif grep -q "is up to date. No migration necessary." flyway.log
+then
+	echo "No migration necessary";
 else
+	cat flyway.log ;
         echo "nok";
         exit 1
 fi
